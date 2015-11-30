7e260d552c22ee18f8ab39110c3bcbf4ac5563cd
瑾宝小公举 <597132100@qq.com>
2015-11-30 23:17:29 +0800

    1、新建工程

---
+++ IYaoSe/IYaoSe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IYaoSe
+//
+//  Created by wangrongchao on 15/11/30.
+//  Copyright © 2015年 truly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IYaoSe/IYaoSe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IYaoSe
+//
+//  Created by wangrongchao on 15/11/30.
+//  Copyright © 2015年 truly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
